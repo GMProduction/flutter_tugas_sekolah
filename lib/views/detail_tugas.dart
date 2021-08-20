1c773f2e1ec058195fc11e8132a20d05ba062079
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2021-08-20 16:41:31 +0700

    detail_tugas

--- lib/views/detail_tugas.dart
+++ lib/views/detail_tugas.dart
@@ -1,23 +1,19 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class DetailMateri extends StatefulWidget {
+class DetailTugas extends StatefulWidget {
   @override
-  _DetailMateriState createState() => _DetailMateriState();
+  _DetailTugasState createState() => _DetailTugasState();
 }
 
-class _DetailMateriState extends State<DetailMateri> {
+class _DetailTugasState extends State<DetailTugas> {
   late VideoPlayerController _controller;
   bool isPlaying = false;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _controller = VideoPlayerController.network(
-        'https://file-examples-com.github.io/uploads/2018/04/file_example_MOV_1280_1_4MB.mov')
+    _controller = VideoPlayerController.network('')
       ..addListener(() {
         setState(() {
           isPlaying = _controller.value.isPlaying;
@@ -30,7 +26,6 @@ class _DetailMateriState extends State<DetailMateri> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _controller.dispose();
   }
@@ -82,7 +77,7 @@ class _DetailMateriState extends State<DetailMateri> {
               margin: EdgeInsets.only(top: 20),
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                "Nama Materi",
+                "Nama Tugas",
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
@@ -93,7 +88,7 @@ class _DetailMateriState extends State<DetailMateri> {
               padding: EdgeInsets.symmetric(horizontal: 20),
               margin: EdgeInsets.only(top: 10),
               child: Text(
-                "Deskripsi Materi, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                "Deskripsi Tugas, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
                 style: TextStyle(
                   fontSize: 14,
                 ),
